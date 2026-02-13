29e8559404a4584366ddea05c4aab7cf6402ba8b
slykhobby27-arch <slykhobby27@gmail.com>
2026-02-13 10:00:00 -0800

    Week 2 - Commit 1: Added dailyEnergyKWh method

---
+++ week1_main.cpp
@@ -0,0 +1,58 @@
+/*
+ * ELECTRICAL LOAD MONITORING SYSTEM
+ * Author: ENOCH TETTEH KWABENA
+ * Index: 01240720D
+ * Course: EEE 227
+ * Week 1: Basic Structure and Appliance Class
+ * Date: February 10, 2026
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Appliance {
+private:
+    string name;
+    double powerRating;
+    double usageHours;
+
+public:
+    // Constructors
+    Appliance() : name(""), powerRating(0), usageHours(0) {}
+    
+    Appliance(string n, double p, double h) {
+        name = n;
+        powerRating = p;
+        usageHours = h;
+    }
+    
+    // Getters
+    string getName() { return name; }
+    double getPower() { return powerRating; }
+    double getHours() { return usageHours; }
+    
+    // Simple display
+    void display() {
+        cout << "Appliance: " << name << ", " 
+             << powerRating << "W, " 
+             << usageHours << " hours/day" << endl;
+    }
+};
+
+int main() {
+    cout << "\n=========================================\n";
+    cout << "   ELECTRICAL LOAD MONITORING SYSTEM\n";
+    cout << "            WEEK 1 - BASIC STRUCTURE\n";
+    cout << "=========================================\n";
+    
+    // Test the class
+    Appliance testApp("Test Appliance", 100, 5);
+    testApp.display();
+    
+    cout << "\nWeek 1 complete: Appliance class created.\n";
+    cout << "Press Enter to exit...";
+    cin.get();
+    
+    return 0;
+}
